9b65f42981608d47dc317524ecc37effe9aeb501
speoxe <speoxe1@gmail.com>
2025-02-05 03:20:00 -0800

    added reports.cpp, fixed all logic and errors, still need to fix border and makefile, along with adding the other files. invmenu, bookinfo, cashier.

--- makefile
+++ makefile
@@ -1,5 +1,7 @@
-run-test: mainmenu.cpp
+main-menu: mainmenu.cpp
 	g++ -std=c++20 -Wall -Werror -o main-menu mainmenu.cpp
+reports: reports.cpp
+	g++ -std=c++20 -Wall -Werror -o main-menu reports.cpp
 
 clean:
 	-rm *.o run-test
--- reports.cpp
+++ reports.cpp
@@ -13,14 +13,17 @@ int main() {
 	cout << "█" << setw(78) << " " << "█";
 	cout << "█" << setw(78) << " " << "█" << endl;
 	cout << "█" << setw(28) << " " << setw(51) << "Serendipity Book Sellers" << "█" << endl;
-	cout << "█" << setw(35) << " " << setw(44) << "Main Menu" << "█" << endl;
+	cout << "█" << setw(35) << " " << setw(44) << "Reports" << "█" << endl;
 
 	cout << "█" << setw(78) << " " << "█" << endl;
 
-	cout << "█" << setw(24) << " " << setw(54) << "1. Cashier Module" << "█" << endl;
-	cout << "█" << setw(24) << " " << setw(54) << "2. Inventory Database Module" << "█" << endl;
-	cout << "█" << setw(24) << " " << setw(54) << "3. Report Module" << "█" << endl;
-	cout << "█" << setw(24) << " " << setw(63) << "\033[31m4. Exit\033[0m " << "█" << endl;
+	cout << "█" << setw(24) << " " << setw(54) << "1. Inventory Listing" << "█" << endl;
+	cout << "█" << setw(24) << " " << setw(54) << "2. Inventory Wholesale Value" << "█" << endl;
+	cout << "█" << setw(24) << " " << setw(54) << "3. Inventory Retail Value" << "█" << endl;
+	cout << "█" << setw(24) << " " << setw(54) << "4. Listing by Quantity" << "█" << endl;
+	cout << "█" << setw(24) << " " << setw(54) << "5. Listing by Cost" << "█" << endl;
+	cout << "█" << setw(24) << " " << setw(54) << "6. Listing by Age" << "█" << endl;
+	cout << "█" << setw(24) << " " << setw(63) << "\033[32m7. Return to Main Menu\033[0m " << "█" << endl;
 	cout << "█" << setw(78) << " " << "█" << endl;
 	cout << "█" << setw(78) << " " << "█" << endl;
 	cout << "█" << setw(78) << " " << "█" << endl;
@@ -62,7 +65,24 @@ do{
 			cout << "Press ENTER to continue ...";
 			cin.ignore(10000, '\n');
 			exit(0);
+
 		case 4:
+			cout << "You selected item 4." << endl;
+			cout << "Press ENTER to continue ...";
+			cin.ignore(10000, '\n');
+			exit(0);
+		case 5:
+			cout << "You selected item 5." << endl;
+			cout << "Press ENTER to continue ...";
+			cin.ignore(10000, '\n');
+			exit(0);
+			break;
+		case 6:
+			cout << "You selected item 6." << endl;
+			cout << "Press ENTER to continue ...";
+			cin.ignore(10000, '\n');
+			exit(0);
+		case 7:
 			exit(0);
 			break;
 		default:
