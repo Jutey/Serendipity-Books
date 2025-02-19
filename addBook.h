97929f475694fcc28580ea2249c9ebafaa30b284
Awesomoose <coxonc406@gmail.com>
2025-02-19 01:01:16 -0800

    mostly complete void addBook function

---
+++ addBook.h
@@ -0,0 +1,9 @@
+#include "bookType.h"
+
+#ifndef ADDBOOK_H
+#define ADDBOOK_H
+
+//same thing here, this vvvvvvv is a filler parameter for the actual array/vector so replace it if need be
+void addBook(bookType &passVectorOrArrayOfStructsHere, int &bookCount);
+
+#endif
