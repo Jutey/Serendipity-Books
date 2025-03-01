94f71e884c0010c2860964fb552b53dd87a0c66b
Awesomoose <coxonc406@gmail.com>
2025-02-28 21:07:25 -0800

    New Doxygen Test 1

--- lookUpBook.h
+++ lookUpBook.h
@@ -23,7 +23,6 @@ using namespace std;
 /// A function called within lookUpBook to clear any tasks or information not being used by the system from the terminal
 ///@returns nothing meaningful for the moment
 void cls();
-<<<<<<< HEAD
 
 /// A function to find a title in the database and give the user a multitude of choices
 ///
@@ -31,8 +30,5 @@ void cls();
 ///@param db: vector<bookType> - the database itself passed into the function to be replicated and searched
 ///@returns nothing meaningful for the moment
 void lookUpBook(vector<bookType> db);
-=======
-int lookUpBook(vector<bookType> db);
->>>>>>> e83689307ed0767201248e917553f1202eb69813
 
 #endif
