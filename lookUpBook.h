bdfabf3a2a5c2b0d8bbec1b1382320db1330154d
Kenny Strawn <kstrawn0@saddleback.edu>
2025-03-03 16:24:28 -0800

    Fix @Awesomoose errors

--- lookUpBook.h
+++ lookUpBook.h
@@ -29,6 +29,6 @@ void cls();
 /// This function returns all matching search results based on the search query entered
 ///@param db: vector<bookType> - the database itself passed into the function to be replicated and searched
 ///@returns nothing meaningful for the moment
-void lookUpBook(vector<bookType> db);
+int lookUpBook(vector<bookType> db);
 
 #endif
