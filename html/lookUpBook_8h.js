a566c3cbb48d044e6cc3455fec48ff7f5a813ac1
Kenny Strawn <kstrawn0@saddleback.edu>
2025-03-03 16:25:30 -0800

    Fix @Awesomoose errors

--- html/lookUpBook_8h.js
+++ html/lookUpBook_8h.js
@@ -1,5 +1,5 @@
 var lookUpBook_8h =
 [
     [ "cls", "lookUpBook_8h.html#a4826277cc150ddc0f4de4bd464a34909", null ],
-    [ "lookUpBook", "lookUpBook_8h.html#a0eacef1197f5dda140f758b394363d2b", null ]
+    [ "lookUpBook", "lookUpBook_8h.html#ab50d0d73b55e4f7b0836934d2b552063", null ]
 ];
--- html/search/all_8.js
+++ html/search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['lookupbook_0',['lookUpBook',['../lookUpBook_8cpp.html#ab50d0d73b55e4f7b0836934d2b552063',1,'lookUpBook(vector&lt; bookType &gt; db):&#160;lookUpBook.cpp'],['../lookUpBook_8h.html#a0eacef1197f5dda140f758b394363d2b',1,'lookUpBook(vector&lt; bookType &gt; db):&#160;lookUpBook.cpp']]],
+  ['lookupbook_0',['lookUpBook',['../lookUpBook_8cpp.html#ab50d0d73b55e4f7b0836934d2b552063',1,'lookUpBook(vector&lt; bookType &gt; db):&#160;lookUpBook.cpp'],['../lookUpBook_8h.html#ab50d0d73b55e4f7b0836934d2b552063',1,'lookUpBook(vector&lt; bookType &gt; db):&#160;lookUpBook.cpp']]],
   ['lookupbook_2ecpp_1',['lookUpBook.cpp',['../lookUpBook_8cpp.html',1,'']]],
   ['lookupbook_2eh_2',['lookUpBook.h',['../lookUpBook_8h.html',1,'']]]
 ];
